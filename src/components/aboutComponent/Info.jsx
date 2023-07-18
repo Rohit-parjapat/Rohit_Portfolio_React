20b33e97ace6d612f1e6bf13801dba59b19dc959
Rohit parjapat <108588569+Rohit-parjapat@users.noreply.github.com>
2023-07-18 18:40:31 +0530

    Update Info.jsx

--- src/components/aboutComponent/Info.jsx
+++ src/components/aboutComponent/Info.jsx
@@ -20,18 +20,8 @@ const MyInfo = () => {
         </div>
         <div className="px-4 flex  w-full" data-aos="fade-up">
           <p className="lg:leading-8 leading-6 lg:text-lg text-md font-family font-normal tracking-wider text-justify my-2 px-5 rounded-md">
-            I am a 3rd-year undergrad in the Bachelor of Computer Application
-            stream. Recently, I completed my Full Stack Web development course
-            from Devtown. I am a NCC (National cadet corps) cadet on Under
-            Officer rank. I am hardworking and I would like to acquire new
-            skills and always passionate to learn new technologies. I can work
-            with HTML5, CSS, JavaScript, PHP, MySQL, NodeJS, ReactJS, ExpressJS,
-            MongoDB and have knowledge of AWS. My current goal is to become a
-            Full Stack Web Developer and I'm working on it. Two Pillars for my
-            learning path are bugs and problem-solving skills. For me, the
-            advancement of technology not only seeks my attention but gives me
-            an abundant amount of knowledge to explore other fields also. I am
-            always ready to give my 100% effort.
+          I am graduated in the Bachelor of Computer Application stream. I have completed my Full Stack Web development Internship from Devtown. I am a NCC (National cadet corps) cadet on Under Officer rank. I am hardworking and I would like to acquire new skills and always passionate to learn new technologies. I can work with HTML5, CSS 3, JavaScript, PHP, MySQL, NodeJS, ReactJS and have knowledge of MongoDB. My current goal is to become a Full Stack Web Developer and I'm working on it. Two Pillars for my learning path are bugs and problem-solving skills. For me, the advancement of technology not only seeks my attention but gives me an abundant amount of knowledge to explore other fields also.
+I am always ready to give my 100%.
           </p>
         </div>
       </div>
